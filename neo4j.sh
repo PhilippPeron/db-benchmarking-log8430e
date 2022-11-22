3e37ced2208cf7093b5c24c1d66188e52abc398e
koenvanoijen <47559666+koenvanoijen@users.noreply.github.com>
2022-11-22 15:51:24 -0500

    Neo4j /home/ubuntu

--- neo4j.sh
+++ neo4j.sh
@@ -1,5 +1,5 @@
-# Install and run MongoDB
-cd ~
+# Install and run Neo4j
+cd /home/ubuntu
 sudo git clone https://github.com/bitnami/containers.git
 cd /bitnami/neo4j/5/debian-11
 sudo docker-compose up -d
