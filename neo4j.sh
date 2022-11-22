cb7f4c63d7db6e638e2b3a7845ce67ebfe1bfa2e
koenvanoijen <47559666+koenvanoijen@users.noreply.github.com>
2022-11-22 16:47:39 -0500

    Install YCSB

--- neo4j.sh
+++ neo4j.sh
@@ -9,10 +9,7 @@ cd ~
 sudo apt install maven
 
 ### Install ycsb
-curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.12.0/ycsb-0.12.0.tar.gz
-tar xfvz ycsb-0.12.0.tar.gz
-cd ycsb-0.12.0
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
+cd ycsb-0.17.0
 
-git clone https://github.com/brianfrankcooper/YCSB.git
-cd YCSB
-mvn -pl com.yahoo.ycsb:neo4j-binding -am clean package
