cfb2889009ca15d6a22a36e47ab23f55d27d661e
koenvanoijen <47559666+koenvanoijen@users.noreply.github.com>
2022-11-22 16:31:12 -0500

    Neo4j go the home page try 5 but hopefull

--- neo4j.sh
+++ neo4j.sh
@@ -1,7 +1,18 @@
 # Install and run Neo4j
 cd ~
 sudo git clone https://github.com/bitnami/containers.git
-ls
 cd containers/bitnami/neo4j/5/debian-11
 sudo docker-compose up -d
 
+#install Maven
+cd ~
+sudo apt install maven
+
+### Install ycsb
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.12.0/ycsb-0.12.0.tar.gz
+tar xfvz ycsb-0.12.0.tar.gz
+cd ycsb-0.12.0
+
+git clone https://github.com/brianfrankcooper/YCSB.git
+cd YCSB
+mvn -pl com.yahoo.ycsb:neo4j-binding -am clean package
