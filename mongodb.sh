d39bc72c25b4e4100145c6e01ec1d891d1f2a910
Philipp Peron <philipp.peron@gmail.com>
2022-11-28 10:24:40 -0500

    Add docker compose

--- mongodb.sh
+++ mongodb.sh
@@ -3,6 +3,63 @@ cd ~
 
 sudo git clone https://github.com/minhhungit/mongodb-cluster-docker-compose.git
 cd mongodb-cluster-docker-compose
+Add
+sudo rm ~/mongodb-cluster-docker-compose/docker-compose.yml
+sudo touch ~/mongodb-cluster-docker-compose/docker-compose.yml
+sudo chmod 777 ~/mongodb-cluster-docker-compose/docker-compose.yml
+sudo cat <<EOF >~/mongodb-cluster-docker-compose/docker-compose.yml
+version: "3.8"
+
+services:
+  mongo1:
+    image: mongo:5
+    container_name: mongo1
+    command: ["--replSet", "my-replica-set", "--bind_ip_all", "--port", "30001"]
+    volumes:
+      - ./data/mongo-1:/data/db
+    ports:
+      - 30001:30001
+    healthcheck:
+      test: test $$(echo "rs.initiate({_id:'my-replica-set',members:[{_id:0,host:\"mongo1:30001\"},{_id:1,host:\"mongo2:30002\"},{_id:2,host:\"mongo3:30003\"}]}).ok || rs.status().ok" | mongo --port 30001 --quiet) -eq 1
+      interval: 10s
+      start_period: 30s
+
+  mongo2:
+    image: mongo:5
+    container_name: mongo2
+    command: ["--replSet", "my-replica-set", "--bind_ip_all", "--port", "30002"]
+    volumes:
+      - ./data/mongo-2:/data/db
+    ports:
+      - 30002:30002
+
+  mongo3:
+    image: mongo:5
+    container_name: mongo3
+    command: ["--replSet", "my-replica-set", "--bind_ip_all", "--port", "30003"]
+    volumes:
+      - ./data/mongo-3:/data/db
+    ports:
+      - 30003:30003
+
+
+
+# docker exec -it mongo1 sh -c "mongo --port 30001"
+
+# rs.initiate(
+#  {
+#    _id : 'my-replica-set',
+#    members: [
+#      { _id : 0, host : "mongo1:30001" },
+#      { _id : 1, host : "mongo2:30002" },
+#      { _id : 2, host : "mongo3:30003" }
+#    ]
+#  }
+#)
+
+# ./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set
+EOF
+
 sudo docker-compose up -d
 
 sudo docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
@@ -43,3 +100,4 @@ cd ~
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.5.0/ycsb-0.5.0.tar.gz
 tar xfvz ycsb-0.5.0.tar.gz
 cd ycsb-0.5.0
+sudo ./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=500000 -p mongodb.url="mongodb://ubuntu:@localhost:27017/admin"
