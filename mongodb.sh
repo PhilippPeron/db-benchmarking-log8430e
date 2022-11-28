31c8e5a00ea4363687b44f75a310a0573ccfd65c
Philipp Peron <philipp.peron@gmail.com>
2022-11-28 10:53:56 -0500

    Change to tutorial

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,25 @@
 # Install and run MongoDB
-cd ~
+sudo apt update
+sudo apt install default-jre
+sudo apt install default-jdk
+sudo apt update -y
+sudp apt install maven -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update -y
+sudo apt install docker-ce -y
 
-sudo git clone https://github.com/minhhungit/mongodb-cluster-docker-compose.git
-cd mongodb-cluster-docker-compose
-Add
+#LOGIN STUFF
+sudo usermod -aG docker ${USER}
+#sudo su - ${USER}
+
+curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+
+
 sudo rm ~/mongodb-cluster-docker-compose/docker-compose.yml
 sudo touch ~/mongodb-cluster-docker-compose/docker-compose.yml
 sudo chmod 777 ~/mongodb-cluster-docker-compose/docker-compose.yml
@@ -59,45 +75,3 @@ services:
 
 # ./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set
 EOF
-
-sudo docker-compose up -d
-
-sudo docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
-sudo docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
-sudo docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
-sudo docker-compose exec shard03-a sh -c "mongosh < /scripts/init-shard03.js"
-
-sudo docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
-
-#sudo docker-compose exec router01 mongosh --port 27017
-#
-#// Enable sharding for database `MyDatabase`
-#sh.enableSharding("MyDatabase")
-#
-#// Setup shardingKey for collection `MyCollection`**
-#db.adminCommand( { shardCollection: "MyDatabase.MyCollection", key: { oemNumber: "hashed", zipCode: 1, supplierId: 1 } } )
-#exit
-
-cd ~
-sudo apt -y install default-jre
-sudo apt -y install default-jdk
-wget http://ftp.heanet.ie/mirrors/www.apache.org/dist/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
-sudo tar xzf apache-maven-*-bin.tar.gz -C /usr/local
-cd /usr/local
-sudo ln -s apache-maven-* maven
-
-sudo touch /etc/profile.d/maven.sh
-sudo chmod 777 /etc/profile.d/maven.sh
-sudo cat <<EOF >/etc/profile.d/maven.sh
-export M2_HOME=/usr/local/maven
-export PATH=${M2_HOME}/bin:${PATH}
-EOF
-source /etc/profile.d/maven.sh
-sudo apt -y install python2
-sudo ln -s /usr/bin/python2 /usr/bin/python
-
-cd ~
-curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.5.0/ycsb-0.5.0.tar.gz
-tar xfvz ycsb-0.5.0.tar.gz
-cd ycsb-0.5.0
-sudo ./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=500000 -p mongodb.url="mongodb://ubuntu:@localhost:27017/admin"
