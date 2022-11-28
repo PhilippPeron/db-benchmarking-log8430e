a092a0573841f0c23712dc413d2fe0de863b471f
Philipp Peron <philipp.peron@gmail.com>
2022-11-28 11:52:57 -0500

    Fix paths

--- mongodb.sh
+++ mongodb.sh
@@ -1,23 +1,26 @@
 # Install and run MongoDB
-sudo apt update
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt update --quiet
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
 sudo apt update -y
-sudp apt install maven -y
+sudo apt install maven -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update -y
 sudo apt install docker-ce -y
+sudo apt -y install python2
+sudo ln -s /usr/bin/python2 /usr/bin/python
 
 #LOGIN STUFF
 sudo usermod -aG docker ${USER}
 #sudo su - ${USER}
 
-curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
 cd ycsb-0.17.0
 mkdir database
 cd database
@@ -26,8 +29,8 @@ cd database
 
 
 sudo touch ./docker-compose-mongodb.yml
-sudo chmod 777 ~/mongodb-cluster-docker-compose/docker-compose.yml
-sudo cat <<EOF >~/mongodb-cluster-docker-compose/docker-compose.yml
+sudo chmod 777 ./docker-compose-mongodb.yml
+sudo cat <<EOF >./docker-compose-mongodb.yml
 version: "3.8"
 
 services:
@@ -81,4 +84,6 @@ services:
 EOF
 
 sudo docker-compose -f docker-compose-mongodb.yml up
-./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=1000 -p mongodb.usert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set
+cd ..
+./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set
+
