136912f45503a1bd4623ff25e98cf4ddc47db527
Philipp Peron <philipp.peron@gmail.com>
2022-11-28 14:50:37 -0500

    Change iterators

--- mongodb.sh
+++ mongodb.sh
@@ -5,17 +5,17 @@ docker pull mongo
 docker network create mongoCluster
 
 # Start the Mongo docker containers
-docker run -d --rm -p 27017:27017 --name mongo0 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo0
-docker run -d --rm -p 27018:27017 --name mongo1 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo1
-docker run -d --rm -p 27019:27017 --name mongo2 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo2
+docker run -d --rm -p 27017:27017 --name mongo1 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo1
+docker run -d --rm -p 27018:27017 --name mongo2 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo2
+docker run -d --rm -p 27019:27017 --name mongo3 --network mongoCluster mongo:5 mongod --replSet myRS --bind_ip localhost,mongo3
 
 # Init replica set
-docker exec -it mongo0 mongosh --eval "rs.initiate({
+docker exec -it mongo1 mongosh --eval "rs.initiate({
  _id: 'myRS',
  members: [
-   {_id: 0, host: 'mongo0:27017'},
-   {_id: 1, host: 'mongo1:27017'},
-   {_id: 2, host: 'mongo2:27017'}
+   {_id: 0, host: 'mongo1:27017'},
+   {_id: 1, host: 'mongo2:27017'},
+   {_id: 2, host: 'mongo3:27017'}
  ]
 })"
 sudo mkdir /home/ubuntu/db-benchmarking-log8430e/results_mongodb
@@ -24,12 +24,12 @@ sudo mkdir /home/ubuntu/db-benchmarking-log8430e/results_mongodb/workloadB
 
 # Run workloads
 cd ycsb-0.17.0
-for iter in {0..2}
+for iter in {1..3}
     do
         ./bin/ycsb.sh load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > /home/ubuntu/db-benchmarking-log8430e/results_mongodb/workloadA/load${iter}
         ./bin/ycsb.sh run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > /home/ubuntu/db-benchmarking-log8430e/results_mongodb/workloadA/run${iter}
     done
-for iter in {0..2}
+for iter in {1..3}
     do
         ./bin/ycsb.sh load mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > /home/ubuntu/db-benchmarking-log8430e/results_mongodb/workloadB/load${iter}
         ./bin/ycsb.sh run mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > /home/ubuntu/db-benchmarking-log8430e/results_mongodb/workloadB/run${iter}
