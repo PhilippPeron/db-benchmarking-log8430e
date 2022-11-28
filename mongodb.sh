be9f6b5aa2d0f19ce21abe316ae911deae3d99ef
Philipp Peron <philipp.peron@gmail.com>
2022-11-28 13:59:47 -0500

    Separate scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,30 +1,4 @@
-# Install and run MongoDB
-sudo apt update --quiet
-sudo apt install default-jre -y
-sudo apt install default-jdk -y
-sudo apt update -y
-sudo apt install maven -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update -y
-sudo apt install docker-ce -y
-sudo apt -y install python2
-sudo ln -s /usr/bin/python2 /usr/bin/python
-
-#Login stuff
-sudo usermod -aG docker ${USER}
-sudo su - ${USER}
-
-# Install docker
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Download and unpack YCSB
-sudo curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
-tar xfvz ycsb-0.17.0.tar.gz
-
-# Get mongo for docker
+# Get mongoDB for docker
 docker pull mongo
 
 # Create a docker network cluster in which the docker containers are visible to each other
@@ -59,7 +33,7 @@ for iter in {0..2}
     done
 
 # Stop all containers
-sudo docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q)
 
 # Delete network
-sudo docker network rm mongoCluster
+docker network rm mongoCluster
