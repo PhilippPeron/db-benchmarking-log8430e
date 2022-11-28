ac4d9f955191bb580c570389aee5613fc907cbc8
koenvanoijen <47559666+koenvanoijen@users.noreply.github.com>
2022-11-28 17:20:04 -0500

    update orientDB.sh

--- orientDB.sh
+++ orientDB.sh
@@ -40,21 +40,27 @@ docker-compose -f docker-compose.orientdb.yml up -d
 cd ..
 cd ycsb-0.17.0
 
-# run orientdb three times on workload a - save output in directory results/orientdb/workload_A
-for iter in {1..3}
-    do
-        bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_A/output_load${iter}
-        bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_A/output_run${iter}
-    done
 
-# run orientdb three times on workload b – save output in directory results/orientdb/workload_B
-for iter in {1..3}
-    do
-        bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_B/output_load${iter}
-        bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_B/output_run${iter}
-    done
+./bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load1.txt
+./bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadB_run1.txt
+./bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load1.txt
+./bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadB_run1.txt
+echo "\nRun complete (1/3). Waiting 60s before next run..."
+sleep 60
 
-cd ../../targetDB/data
+./bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load2.txt
+./bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadB_run2.txt
+./bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load2.txt
+./bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_run2.txt
+echo "\nRun complete (2/3). Waiting 60s before next run..."
+sleep 60
+
+./bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load3.txt
+./bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadB_run3.txt
+./bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_load3.txt
+./bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results_orientdb/workloadA_run3.txt
+echo "\Run Complete"
+
 
 # stop services defined in docker-compose-orientdb.yml
 docker-compose -f docker-compose-orientdb.yml down
